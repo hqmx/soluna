5fd5a5a210888644b70b51e084d481f5b0ac6097
hqmx <support@hqmx.net>
2025-11-25 06:20:11 +0700

    fix: Remove next-video dependency to resolve bundle size issue

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-import { withNextVideo } from 'next-video/process';
 
 const nextConfig: NextConfig = {
   images: {
@@ -13,4 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
